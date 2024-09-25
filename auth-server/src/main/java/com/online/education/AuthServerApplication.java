178c0b997e1c2eb524b0e34c51bc832a5a31b9b3
Ali Ahmed <aliahmed3195@gmail.com>
2024-09-25 16:25:42 +0500

    commit

--- auth-server/src/main/java/com/online/education/AuthServerApplication.java
+++ auth-server/src/main/java/com/online/education/AuthServerApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
